7c0e4407852f80d0b2e3c2efbfcf4a6fd2988598
hamzasehouli <sehoulihamza@yahoo.com>
2021-12-22 16:53:30 +0100

    Finished signup login

--- client/src/views/Signup.js
+++ client/src/views/Signup.js
@@ -1,9 +1,33 @@
 import React, { Component } from "react";
 
 export default class Signup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+    };
+  }
+
+  state = { name: "µdd" };
+
+  submitForm = (e) => {
+    e.preventDefault();
+    console.log(this);
+  };
+
+  onChange = (e) => {
+    console.log(e);
+  };
   render() {
     return (
-      <form method="post" action="/api/v1/auth/signup" class="form">
+      <form
+        onSubmit={this.submitForm}
+        method="post"
+        action="/api/v1/auth/signup"
+        class="form"
+      >
         <div class="form__control">
           <label for="name" class="form__label">
             Username
@@ -13,6 +37,8 @@ export default class Signup extends Component {
             type="text"
             id="name"
             class="form__input"
+            value={this.state.username}
+            onInput={(e) => this.setState({ username: e.target.value })}
           ></input>
         </div>
         <div class="form__control">
@@ -24,6 +50,8 @@ export default class Signup extends Component {
             type="email"
             id="email"
             class="form__input"
+            value={this.state.email}
+            onInput={(e) => this.setState({ email: e.target.value })}
           ></input>
         </div>
         <div class="form__control">
@@ -35,6 +63,8 @@ export default class Signup extends Component {
             type="password"
             id="password"
             class="form__input"
+            value={this.state.password}
+            onInput={(e) => this.setState({ password: e.target.value })}
           ></input>
         </div>
         <button type="submit" class="btn btn-flat">
