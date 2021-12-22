6f7ca469ce00b61ccdea976ef178993c49f35701
Hamza Sehouli <sehouli.hamza@gmail.com>
2021-12-22 20:57:57 +0100

    Finished login functionalitites in the login view

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      password: "",
+      email: "",
+    };
+  }
+
+  onSubmit = async (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    const body = { email: this.state.email, password: this.state.password };
+    const res = await fetch("http://localhost:8000/api/v1/auth/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await res.json();
+    console.log(data);
+    console.log(res);
+  };
+
   render() {
     return (
-      <form method="post" action="/api/v1/auth/login" className="form">
+      <form onSubmit={this.onSubmit} className="form">
         <div className="form__control">
-          <label for="email" className="form__label">
+          <label htmlFor="email" className="form__label">
             Email
           </label>
           <input
@@ -13,17 +37,21 @@ export default class Login extends Component {
             type="email"
             id="email"
             className="form__input"
+            value={this.state.email}
+            onInput={(e) => this.setState({ email: e.target.value })}
           ></input>
         </div>
         <div className="form__control">
-          <label for="email" className="form__label">
+          <label htmlFor="email" className="form__label">
             Password
           </label>
           <input
             name="password"
             type="password"
             id="password"
+            value={this.state.password}
             className="form__input"
+            onInput={(e) => this.setState({ password: e.target.value })}
           ></input>
         </div>
         <button type="submit" className="btn btn-flat">
--- client/src/views/Signup.js
+++ client/src/views/Signup.js
@@ -37,48 +37,48 @@ export default class Signup extends Component {
         onSubmit={this.submitForm}
         method="post"
         action="/api/v1/auth/signup"
-        class="form"
+        className="form"
       >
-        <div class="form__control">
-          <label for="name" class="form__label">
+        <div className="form__control">
+          <label htmlFor="name" className="form__label">
             Username
           </label>
           <input
             name="username"
             type="text"
             id="name"
-            class="form__input"
+            className="form__input"
             value={this.state.username}
             onInput={(e) => this.setState({ username: e.target.value })}
           ></input>
         </div>
-        <div class="form__control">
-          <label for="email" class="form__label">
+        <div className="form__control">
+          <label htmlFor="email" className="form__label">
             Email
           </label>
           <input
             name="email"
             type="email"
             id="email"
-            class="form__input"
+            className="form__input"
             value={this.state.email}
             onInput={(e) => this.setState({ email: e.target.value })}
           ></input>
         </div>
-        <div class="form__control">
-          <label for="email" class="form__label">
+        <div className="form__control">
+          <label htmlFor="email" className="form__label">
             Password
           </label>
           <input
             name="password"
             type="password"
             id="password"
-            class="form__input"
+            className="form__input"
             value={this.state.password}
             onInput={(e) => this.setState({ password: e.target.value })}
           ></input>
         </div>
-        <button type="submit" class="btn btn-flat">
+        <button type="submit" className="btn btn-flat">
           Signup
         </button>
         <a href="/login">You do not have an account yet? Signup here</a>
